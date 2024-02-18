3cc00160c70b23c60240d90bb0e6543418ec8a89
Eko Prasetyo <eko.prasetyo.38013@gmail.com>
2024-02-19 02:45:07 +0800

    feat: delete all and paste new source code from freecodecamp

--- number_guess.sh
+++ number_guess.sh
@@ -25,43 +25,27 @@ then
 
     # insert new player into players table
     INSERT_NEW_PLAYER=$($PSQL "INSERT INTO players (name) VALUES ('$USERNAME')")
-
-    # # get player id
-    # PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name LIKE '$USERNAME'")
-
-    # # insert new game
-    # INSERT_NEW_GAME=$($PSQL "INSERT INTO games (player_id) VALUES ($PLAYER_ID)")
   else
-    # # get player id
-    # PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name LIKE '$USERNAME'")
-
-    # # insert new game
-    # INSERT_NEW_GAME=$($PSQL "INSERT INTO games (player_id) VALUES ($PLAYER_ID)")
-
     # get total played games
-    TOTAL_PLAYED_GAMES=$($PSQL "SELECT COUNT(*) FROM players INNER JOIN games USING (player_id) WHERE name = '$GET_USERNAME_RESULT'")
-    echo "TOTAL PLAYED GAMES : $TOTAL_PLAYED_GAMES"
+    TOTAL_PLAYED_GAMES=$($PSQL "SELECT COUNT(*) FROM players INNER JOIN games USING (player_id) WHERE name = '$USERNAME'")
+    # echo "TOTAL PLAYED GAMES : $TOTAL_PLAYED_GAMES"
 
     # get best game
-    BEST_GAME=$($PSQL "SELECT number_of_tries FROM (SELECT * FROM players INNER JOIN games USING (player_id)) t WHERE name = 'Eko' ORDER BY number_of_tries LIMIT 1")
+    BEST_GAME=$($PSQL "SELECT number_of_tries FROM (SELECT * FROM players INNER JOIN games USING (player_id)) t WHERE name = '$USERNAME' ORDER BY number_of_tries LIMIT 1")
 
-    echo "Welcome back,$GET_USERNAME_RESULT! You have played $TOTAL_PLAYED_GAMES games, and your best game took $BEST_GAME guesses." | tr -s ' ' | sed 's/^ *//;s/ *$//'
+    echo "Welcome back,$USERNAME! You have played $TOTAL_PLAYED_GAMES games, and your best game took $BEST_GAME guesses." | tr -s ' ' | sed 's/^ *//;s/ *$//'
   fi
-  
+
   # get player id
   PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name LIKE '$USERNAME'")
 
-  # insert new game
-  INSERT_NEW_GAME=$($PSQL "INSERT INTO games (player_id) VALUES ($PLAYER_ID)")
-  
-
-  # get game id
-  GAME_ID=$($PSQL "SELECT game_id FROM games WHERE player_id = $PLAYER_ID ORDER BY game_id DESC LIMIT 1")
-
+  # set number of try value
   NUMBER_OF_TRY=1
+
   # accept guessed number
   echo -e "\nGuess the secret number between 1 and 1000:"
   read GUESSED_NUMBER
+
   while true
   do
     # check if the answer is integer or not
@@ -72,24 +56,20 @@ then
         echo -e "\nIt's lower than that, guess again:"
         read GUESSED_NUMBER
         NUMBER_OF_TRY=$((NUMBER_OF_TRY+1))
-
-        # update number of tries column
-        UPDATE_NUMBER_TRIES=$($PSQL "UPDATE games SET number_of_tries=$NUMBER_OF_TRY WHERE game_id=$GAME_ID")
-
       elif [[ $GUESSED_NUMBER -gt $RANDOM_NUMBER ]]
       then
         echo -e "\nIt's higher than that, guess again:"
         read GUESSED_NUMBER
         NUMBER_OF_TRY=$((NUMBER_OF_TRY+1))
-
-        # update number of tries column
-        UPDATE_NUMBER_TRIES=$($PSQL "UPDATE games SET number_of_tries=$NUMBER_OF_TRY WHERE game_id=$GAME_ID")
       else
         echo -e "\nYou guessed it in $NUMBER_OF_TRY tries. The secret number was $RANDOM_NUMBER. Nice job!" | tr -s ' ' | sed 's/^ *//;s/ *$//'
+
+        # add new record to game
+        INSERT_NEW_GAME=$($PSQL "INSERT INTO games (player_id,number_of_tries) VALUES ($PLAYER_ID,$NUMBER_OF_TRY)")
         break
       fi
     else
-      # if not
+      # if the guessed number not an integer
       echo -e "That is not an integer, guess again:"
       read GUESSED_NUMBER
     fi
